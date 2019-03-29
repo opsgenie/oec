d2c45fbf0118dd9f920b1bfbf604f6ae36702426
Metehan Öztürk <mozturk@atlassian.com>
2019-03-29 11:11:12 +0300

    Added OEC Windows Service, updated Gopkg files and Dockerfile

--- OSC.md
+++ OSC.md
@@ -80,7 +80,9 @@
 
    20.1. Available under license
 
+21. github.com/kardianos/service
 
+   21.1. Available under license
 
 1. github.com/aws/aws-sdk-go)
    1.1. Available under license
@@ -2015,3 +2017,27 @@ copies or substantial portions of the Software.
 
 
 THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+21. github.com/kardianos/service
+   21.1. Available under license
+   
+Copyright (c) 2015 Daniel Theophanes
+
+This software is provided 'as-is', without any express or implied
+warranty. In no event will the authors be held liable for any damages
+arising from the use of this software.
+
+Permission is granted to anyone to use this software for any purpose,
+including commercial applications, and to alter it and redistribute it
+freely, subject to the following restrictions:
+
+   1. The origin of this software must not be misrepresented; you must not
+   claim that you wrote the original software. If you use this software
+   in a product, an acknowledgment in the product documentation would be
+   appreciated but is not required.
+
+   2. Altered source versions must be plainly marked as such, and must not be
+   misrepresented as being the original software.
+
+   3. This notice may not be removed or altered from any source
+   distribution.
---
+++ windows_service/main/oecService.go
@@ -0,0 +1,256 @@
+// +build windows
+
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/kardianos/service"
+	"log"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"strings"
+	"syscall"
+)
+
+type Config struct {
+	Name, DisplayName, Description string
+
+	OECPath string
+	Args    []string
+	Env     []string
+
+	Stderr, Stdout string
+}
+
+var logger service.Logger
+
+type Program struct {
+	*Config
+
+	service service.Service
+	cmd     *exec.Cmd
+
+	exit chan struct{}
+}
+
+func (p *Program) Start(s service.Service) error {
+	p.cmd = exec.Command("cmd", append([]string{"/C", p.OECPath}, p.Args...)...)
+	p.cmd.Env = append(os.Environ(), p.Env...)
+
+	p.exit = make(chan struct{})
+
+	go p.run()
+	return nil
+}
+func (p *Program) run() {
+	logger.Info("Starting ", p.DisplayName)
+	defer func() {
+		if service.Interactive() {
+			p.Stop(p.service)
+		} else {
+			p.service.Stop()
+		}
+	}()
+
+	if p.Stderr != "" {
+		file, err := os.OpenFile(p.Stderr, os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0777)
+		if err != nil {
+			logger.Warningf("Failed to open std err %q: %v", p.Stderr, err)
+			return
+		}
+		defer file.Close()
+		p.cmd.Stderr = file
+	}
+	if p.Stdout != "" {
+		file, err := os.OpenFile(p.Stdout, os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0777)
+		if err != nil {
+			logger.Warningf("Failed to open std out %q: %v", p.Stdout, err)
+			return
+		}
+		defer file.Close()
+		p.cmd.Stdout = file
+	}
+
+	unexpectedExit := make(chan struct{})
+	go func() {
+		select {
+		case <-unexpectedExit:
+		case <-p.exit:
+			sendCtrlCEvent(p.cmd.Process.Pid, p.Stderr)
+		}
+	}()
+
+	err := p.cmd.Run()
+	if err != nil {
+		logger.Warningf("Error running: %v", err)
+	}
+
+	close(unexpectedExit)
+	return
+}
+
+func errMessageCtrlC(errType string, err error) []byte {
+	return []byte(fmt.Sprintf("Failed while stopping the service. %s error: %v\n", errType, err))
+}
+
+func sendCtrlCEvent(pid int, stderr string) {
+	file, err := os.OpenFile(stderr, os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0777)
+	if err != nil {
+		logger.Warningf("Failed to open std out %q: %v", stderr, err)
+	}
+	defer file.Close()
+
+	kernel32, err := syscall.LoadDLL("kernel32.dll")
+	if err != nil {
+		file.Write(errMessageCtrlC("LoadDLL", err))
+	}
+
+	freeConsole, err := kernel32.FindProc("FreeConsole")
+	if err != nil {
+		file.Write(errMessageCtrlC("FindProc[FreeConsole]", err))
+	}
+	r, _, err := freeConsole.Call()
+	if r == 0 {
+		file.Write(errMessageCtrlC("FreeConsole", err))
+	}
+
+	attachConsole, err := kernel32.FindProc("AttachConsole")
+	if err != nil {
+		file.Write(errMessageCtrlC("FindProc[AttachConsole]", err))
+	}
+	r, _, err = attachConsole.Call(uintptr(pid))
+	if r == 0 {
+		file.Write(errMessageCtrlC("AttachConsole", err))
+	}
+
+	setConsoleCtrlHandler, e := kernel32.FindProc("SetConsoleCtrlHandler")
+	if e != nil {
+		file.Write(errMessageCtrlC("FindProc[SetConsoleCtrlHandler]", err))
+	}
+	r, _, e = setConsoleCtrlHandler.Call(0, 1)
+	if r == 0 {
+		file.Write(errMessageCtrlC("SetConsoleCtrlHandler", err))
+	}
+
+	generateConsoleCtrlEvent, err := kernel32.FindProc("GenerateConsoleCtrlEvent")
+	if err != nil {
+		file.Write(errMessageCtrlC("FindProc[GenerateConsoleCtrlEvent]", err))
+	}
+	r, _, err = generateConsoleCtrlEvent.Call(syscall.CTRL_C_EVENT, uintptr(pid))
+	if r == 0 {
+		file.Write(errMessageCtrlC("GenerateConsoleCtrlEvent", err))
+	}
+
+	r, _, err = freeConsole.Call()
+	if r == 0 {
+		file.Write(errMessageCtrlC("FreeConsole", err))
+	}
+	r, _, e = setConsoleCtrlHandler.Call(0, 0)
+	if r == 0 {
+		file.Write(errMessageCtrlC("SetConsoleCtrlHandler", err))
+	}
+}
+
+func (p *Program) Stop(s service.Service) error {
+	close(p.exit)
+
+	return nil
+}
+
+func getConfigPath() (string, error) {
+	exePath, err := os.Executable()
+	if err != nil {
+		return "", err
+	}
+
+	ext := filepath.Ext(exePath)
+	configPath := exePath[:len(exePath)-len(ext)] + ".json"
+
+	return configPath, nil
+}
+
+func getConfig(path string) (*Config, error) {
+	file, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	conf := &Config{}
+
+	err = json.NewDecoder(file).Decode(&conf)
+	if err != nil {
+		return nil, err
+	}
+	return conf, nil
+}
+
+func main() {
+
+	configPath, err := getConfigPath()
+	if err != nil {
+		log.Fatal(err)
+	}
+	config, err := getConfig(configPath)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	svcConfig := &service.Config{
+		Name:        config.Name,
+		DisplayName: config.DisplayName,
+		Description: config.Description,
+	}
+
+	prg := &Program{
+		Config: config,
+	}
+
+	svc, err := service.New(prg, svcConfig)
+	if err != nil {
+		log.Fatal(err)
+	}
+	prg.service = svc
+
+	errs := make(chan error, 5)
+	logger, err = svc.Logger(errs)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	go func() {
+		for {
+			err := <-errs
+			if err != nil {
+				log.Print(err)
+			}
+		}
+	}()
+
+	if len(os.Args) > 1 {
+		action := os.Args[1]
+
+		err := service.Control(svc, action)
+		if err != nil {
+			if strings.Contains(err.Error(), "Unknown action") {
+				log.Println(err)
+				log.Fatalf("Valid actions: %q\n", service.ControlAction)
+			}
+			log.Fatal(err)
+		}
+		if action == "stop" {
+			log.Println("OEC service stopped successfully.")
+		} else {
+			log.Println("OEC service " + action + "ed successfully.")
+		}
+
+		return
+	}
+
+	err = svc.Run()
+	if err != nil {
+		logger.Error(err)
+	}
+}
