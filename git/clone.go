7c3cfcecb87c46f4a535fbad6235af878aa36900
Cem Küçük <ckucuk@atlassian.com>
2020-12-29 11:56:57 +0300

    Upgrade dependency versions

--- git/clone.go
+++ git/clone.go
@@ -1,9 +1,9 @@
 package git
 
 import (
-	"gopkg.in/src-d/go-git.v4"
-	"gopkg.in/src-d/go-git.v4/plumbing"
-	"gopkg.in/src-d/go-git.v4/plumbing/transport/ssh"
+	"github.com/go-git/go-git/v5"
+	"github.com/go-git/go-git/v5/plumbing"
+	"github.com/go-git/go-git/v5/plumbing/transport/ssh"
 	"io/ioutil"
 	"os"
 )
